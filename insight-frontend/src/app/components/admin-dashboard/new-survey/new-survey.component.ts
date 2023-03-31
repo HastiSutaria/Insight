a5cb3a5e2ba18acfc712be320120c91b8055587c
HastiSutaria <hastisutaria.ict19@gmail.com>
2023-03-31 12:19:47 +0530

    form model

--- insight-frontend/src/app/components/admin-dashboard/new-survey/new-survey.component.ts
+++ insight-frontend/src/app/components/admin-dashboard/new-survey/new-survey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -7,15 +8,83 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./new-survey.component.css'],
 })
 export class NewSurveyComponent implements OnInit {
-  name: String = '';
+  formName: String = '';
   description: String = '';
+  dynamicForm!: FormGroup;
+  surveyForm!: NgForm;
+  FinalBody: any = {};
+  addingInputElement: Boolean = false;
+  dynamicInputs = [];
+  
+  inputTypes: string[] = ['paragraph','date', 'radio', 'select'];
+
   constructor( private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
       console.log(params['name'], params['description']);
-      this.name = params['name'];
+      this.formName = params['name'];
       this.description = params['description'];
     });
+
+    this.dynamicForm = new FormGroup({
+      // 'name': new FormControl(null),
+      'question': new FormControl(null, Validators.required),
+      'type': new FormControl(null, [Validators.required]),
+      'options': new FormArray([], Validators.required)
+    })
   }
+
+  onAddInputElement(){
+    this.addingInputElement = true;
+    if (this.dynamicInputs.length < 1) {
+      this.formName = this.dynamicForm.value.name;
+    }
+    
+
+    setTimeout(() => {
+      this.addingInputElement = false;
+      this.dynamicInputs.push({
+        label: this.dynamicForm.value.question,
+        type: this.dynamicForm.value.type,
+        options: this.dynamicForm.value?.options
+
+      });
+      this.dynamicForm.reset();
+    }, 500);
+
+  }
+
+  onAddOption() {
+    const formControl = new FormControl(null);
+    (<FormArray>this.dynamicForm.get('options')).push(formControl);
+  }
+  onSubmitSurveyForm(surveyForm: NgForm) {
+    console.log(this.formName)
+    this.FinalBody.name = this.formName
+    this.FinalBody.dynamicInputs = this.dynamicInputs;
+    // this.http.post('http://localhost:3000/create/form', this.FinalBody, { observe: 'body' }).subscribe((response: any) => {
+    //   let dialogRef = this.dialog.open(DialogElementsExampleDialog, {
+    //     data: {
+    //       url: window.location.origin+`/forms/${response.slug}/view/form`
+          
+    //     },
+    //     height: '400px',
+    //     width: '600px',
+    //   });
+    //   dialogRef.afterClosed().subscribe(result => {
+    //     this.route.navigate(['/']);
+    //   });
+    // }, (error) => {
+    //   this.snackbar.open(error.message, "Dismiss", {
+    //     duration: 2000
+    //   })
+    // })
+  }
+
+  onDeleteOption(index: number) {
+    (<FormArray>this.dynamicForm.get('options')).removeAt(index);
+  }
+
+  
 }
