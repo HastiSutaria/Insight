a6306be2dd5389cd08dc634ea9dc03f2fc23a35a
HastiSutaria <hastisutaria.ict19@gmail.com>
2023-04-05 15:31:11 +0530

    conflict resolved

--- insight-frontend/src/app/services/form-data.service.ts
+++ insight-frontend/src/app/services/form-data.service.ts
@@ -15,22 +15,6 @@ export class FormDataService {
     const create_form_url = 'http://localhost:3000/create/survey';
     console.log('in save form data', create_form_url);
     return this.http.post<any>(create_form_url, formData, {observe: 'body'});
-    // this.http.post('http://localhost:3000/create/form', this.FinalBody, { observe: 'body' }).subscribe((response: any) => {
-    //     let dialogRef = this.dialog.open(DialogElementsExampleDialog, {
-    //       data: {
-    //         url: window.location.origin+`/forms/${response.slug}/view/form`
-            
-    //       },
-    //       height: '400px',
-    //       width: '600px',
-    //     });
-    //     dialogRef.afterClosed().subscribe(result => {
-    //       this.route.navigate(['/']);
-    //     });
-    //   }, (error) => {
-    //     this.snackbar.open(error.message, "Dismiss", {
-    //       duration: 2000
-    //     })
-    //   })
+
   }
 }
